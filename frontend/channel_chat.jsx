b0c288c7d8917d6cc0c08bcbc3acc036e7aa68ce
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-01-27 16:16:15 -0600

    Create session reducers and configure store

--- frontend/channel_chat.jsx
+++ frontend/channel_chat.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 // Testing start
 import * as sessionApiUtil from './util/session_api_util';
 // Testing end
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   // Testing start
   window.login = sessionApiUtil.login;
   window.signup = sessionApiUtil.signup;
   window.logout = sessionApiUtil.logout;
-  //Testing end
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // Testing end
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to channelchat</h1>, root);
 });
