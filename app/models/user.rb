0efe295197494b86d8110779957102f6480c95f7
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-01-27 12:50:10 -0600

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  attr_reader: :password
+
+  validates :username, :password_digest, :email, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, allow_nil: true, length: { minimum: 6 }
 end
