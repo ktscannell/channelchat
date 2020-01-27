fc961f205aa1d5ddc0acc3c150ef011d703b1e1e
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-01-27 17:50:54 -0600

    Create greeting and session_form components

--- frontend/channel_chat.jsx
+++ frontend/channel_chat.jsx
@@ -1,20 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import configureStore from "./store/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 // Testing start
-import * as sessionApiUtil from './util/session_api_util';
+// import * as sessionApiUtil from './util/session_api_util';
+import { signup, login, logout } from './actions/session_actions';
 // Testing end
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   // Testing start
-  window.login = sessionApiUtil.login;
-  window.signup = sessionApiUtil.signup;
-  window.logout = sessionApiUtil.logout;
+  // window.login = sessionApiUtil.login;
+  // window.signup = sessionApiUtil.signup;
+  // window.logout = sessionApiUtil.logout;
+
+  window.login = login;
 
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   // Testing end
-  const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to channelchat</h1>, root);
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 });
