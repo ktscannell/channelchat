10739e46c51e037f32ece51fb64aab8f735dd2a7
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-02-04 12:59:43 -0600

    Configure actioncable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://channel-chat-heroku.herokuapp.com', 'http://channel-chat-heroku.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://channel-chat-heroku.herokuapp.com/cable" 
 end
