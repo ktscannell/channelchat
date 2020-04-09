3cd38450453853f05eaf7d9214956648394cc0f1
Kieran Scannell <kierscan@gmail.com>
2020-04-09 10:35:36 -0500

    Add randomly generated colors for profile icons

--- frontend/components/channel_show/message_detail.jsx
+++ frontend/components/channel_show/message_detail.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const MessageDetail = ({ message, author }) => (
   <li className="message-container">
-    <img src={window.accountURL} />
+    <i className="fas fa-user" style={{backgroundColor: generateRandomColor()}}></i>
     <div className="message-text-container">
       <h4>{author.username}</h4>
       <p>{message.body}</p>
     </div>
   </li>
 );
+
+const generateRandomColor = () => (
+  '#' + Math.floor(Math.random() * 16777215).toString(16)
+)
 
 export default MessageDetail;
